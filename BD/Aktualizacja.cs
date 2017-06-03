b2d23f97a7c2e5a57df9f9714709338d16f9a6c8
Pawel <pawel190x@gmail.com>
2017-06-03 17:35:28 +0200

    Aktualizacja - wykrycie ostatniej zmiany w bazie

---
+++ BD/Aktualizacja.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+namespace BD
+{
+    class Aktualizacja
+    {
+        private string lastupdate;
+
+        public string ostatniaAktualizacja
+        {
+            get
+            {
+                return this.lastupdate;
+            }
+            set
+            {
+                this.lastupdate = value;
+            }
+        }
+
+        public string dataOstatniejAktualizacji()
+        {
+            Polacz_z_baza _polacz = new Polacz_z_baza();
+            SqlConnection _polaczenie = _polacz.PolaczZBaza();
+            SqlCommand _zapytanie = _polacz.UtworzZapytanie("select last_user_update from sys.dm_db_index_usage_stats where database_id = DB_ID('baza') order by last_user_update desc");
+            SqlDataReader reader = _zapytanie.ExecuteReader();
+            reader.Read();
+            var tmpupdate = reader["last_user_update"].ToString();
+            reader.Close();
+            return tmpupdate;
+        }
+
+        public bool czyBylaAktualizacja()
+        {
+            if (this.lastupdate.Equals(dataOstatniejAktualizacji()))
+                return false;
+            else
+            {
+                this.lastupdate = dataOstatniejAktualizacji();
+                return true;
+            }
+        }
+    }
+}
